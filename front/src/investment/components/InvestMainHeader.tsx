4a392f5f9c4f7ca36c162886961707c91fbdc66f
이예지 <lllyyj75@gmail.com>
2024-09-21 17:30:15 +0900

    Feat : navlink로 자동 투자 가도록 링크

--- front/src/investment/components/InvestMainHeader.tsx
+++ front/src/investment/components/InvestMainHeader.tsx
@@ -1,15 +1,18 @@
 import React from "react";
+import { NavLink } from 'react-router-dom';
 
 const InvestMainHeader: React.FC = () => {
   return (
     <div className="flex justify-between items-center w-screen p-4 bg-customDarkGreen text-white rounded-b-lg">
       <h1 className="text-4xl font-bold mr-auto">투자</h1>
-      <button className="bg-green-100 text-green-900 font-semibold px-4 py-3 rounded-full shadow-md flex items-center">
-        나의 자동투자
-        <span className="bg-customDarkGreen text-white rounded-full px-3 py-1 ml-2">
-          GO
-        </span>
-      </button>
+      <NavLink to="/auto-invest" className="inline-block">
+        <button className="bg-green-100 text-green-900 font-semibold px-4 py-3 rounded-full shadow-md flex items-center">
+          나의 자동투자
+          <span className="bg-customDarkGreen text-white rounded-full px-3 py-1 ml-2">
+            GO
+          </span>
+        </button>
+      </NavLink>
     </div>
   );
 };
