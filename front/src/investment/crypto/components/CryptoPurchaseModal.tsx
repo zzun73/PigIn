4d40b0cbee6d9fe30e39425a9f26162940585f29
piilll <spchoi1998@naver.com>
2024-09-23 15:26:04 +0900

    Add : 주식 매수 모달 추가, 매도 모달 위치에 자동투자 포트폴리오 버튼 위치

---
+++ front/src/investment/crypto/components/CryptoPurchaseModal.tsx
@@ -0,0 +1,141 @@
+import React, { useEffect, useCallback } from "react";
+import { CgClose } from "react-icons/cg";
+
+interface CryptoPurchaseModalProps {
+  inputValue: string;
+  setInputValue: React.Dispatch<React.SetStateAction<string>>;
+  onClose: () => void;
+  cryptoName: string;
+  cryptoPrice: number;
+}
+
+const CryptoPurchaseModal: React.FC<CryptoPurchaseModalProps> = ({
+  inputValue,
+  setInputValue,
+  onClose,
+  cryptoName,
+  cryptoPrice,
+}) => {
+  const handleKeypadClick = (number: string) => {
+    setInputValue((prev) => {
+      if (prev.length < 5) {
+        return prev + number;
+      } else {
+        return prev;
+      }
+    });
+  };
+
+  const handleBackspace = useCallback(() => {
+    setInputValue((prev) => prev.slice(0, -1));
+  }, [setInputValue]);
+
+  const handleAddAmount = (amount: number) => {
+    setInputValue((prev) => {
+      const newValue = parseInt(prev || "0") + amount;
+      return newValue.toString();
+    });
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Backspace") {
+        handleBackspace();
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [handleBackspace]);
+
+  const inputAmount = parseFloat(inputValue) || 0;
+  const percentage = ((inputAmount / cryptoPrice) * 100).toFixed(2);
+
+  return (
+    <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-end z-50">
+      <div className="bg-white w-full h-3/4 rounded-t-3xl p-6 relative">
+        {/* 모달 상단 */}
+        <div className="flex justify-between items-center mb-4">
+          <h1 className="text-2xl font-bold text-black flex justify-center">
+            얼마를 매수하시겠어요?
+          </h1>
+          <div onClick={onClose} className="text-black">
+            <CgClose size={24} />
+          </div>
+        </div>
+
+        <div className="text-lg text-center text-black mb-4">
+          {cryptoName} 현재가 : {cryptoPrice.toLocaleString()}원
+        </div>
+
+        {/* 가격 표시 칸 */}
+        <div className="relative flex justify-center mb-6">
+          <input
+            type="text"
+            value={inputValue}
+            readOnly
+            className={`bg-transparent text-center text-black text-3xl w-3/4 p-2 transition-all ${
+              inputValue ? "border-b border-black" : ""
+            }`}
+          />
+          <div
+            className={`absolute right-4 mt-3 flex items-center space-x-1 ${
+              inputValue ? "text-black" : ""
+            }`}
+          >
+            <span className="text-xl">원 ({percentage}%)</span>
+          </div>
+        </div>
+
+        {/* 500, 1000, 3000, 5000원 추가 버튼 */}
+        <div className="flex justify-center space-x-4 mb-6">
+          {[
+            { label: "+500원", value: 500 },
+            { label: "+1000원", value: 1000 },
+            { label: "+3000원", value: 3000 },
+            { label: "+5000원", value: 5000 },
+          ].map((button) => (
+            <button
+              key={button.value}
+              className="bg-customDarkGreen p-2 text-sm rounded-full  transition-colors"
+              onClick={() => handleAddAmount(button.value)}
+            >
+              {button.label}
+            </button>
+          ))}
+        </div>
+
+        {/* 키패드 */}
+        <div className="grid grid-cols-3 gap-4 justify-center">
+          {["1", "2", "3", "4", "5", "6", "7", "8", "9"].map((num) => (
+            <button
+              key={num}
+              className="bg-transparent text-black text-2xl p-4 rounded-lg transition-colors w-full h-20"
+              onClick={() => handleKeypadClick(num)}
+            >
+              {num}
+            </button>
+          ))}
+          <button
+            className="bg-transparent text-black text-2xl p-4 rounded-lg transition-colors w-full h-20 col-span-3"
+            onClick={() => handleKeypadClick("0")}
+          >
+            0
+          </button>
+        </div>
+
+        {/* 매수하기 버튼 */}
+        <div className="flex justify-center mt-6">
+          <button className="bg-green-500 text-white w-3/4 py-3 rounded-full text-lg font-bold">
+            매수하기
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CryptoPurchaseModal;
--- front/src/investment/stock/pages/StockDetailPage.tsx
+++ front/src/investment/stock/pages/StockDetailPage.tsx
@@ -1,16 +1,12 @@
 import React, { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
-import {
-  AreaChart,
-  XAxis,
-  YAxis,
-  Area,
-  ResponsiveContainer,
-  Tooltip,
-} from "recharts";
 import { FaRegHeart, FaHeart } from "react-icons/fa";
 import { CgChevronLeft } from "react-icons/cg";
 import { StockItemData } from "../../interfaces/StockInterface";
+import StockDetailGraph from "../components/StockDetailGraph";
+import StockDetailInfo from "../components/StockDetailInfo";
+import StockPurchaseModal from "../components/StockPurchaseModal";
+import StockNews from "../components/StockNews";
 
 const StockDetailPage: React.FC = () => {
   const navigate = useNavigate();
@@ -20,6 +16,8 @@ const StockDetailPage: React.FC = () => {
   const [selectedInfoType, setSelectedInfoType] = useState<string>("상세정보");
 
   const [isLiked, setIsLiked] = useState(false);
+  const [isBuyModalVisible, setIsBuyModalVisible] = useState(false);
+  const [inputValue, setInputValue] = useState<string>("");
 
   const handleBackClick = () => {
     navigate(-1);
@@ -39,6 +37,15 @@ const StockDetailPage: React.FC = () => {
 
   const handleHeartClick = () => {
     setIsLiked((prevLiked) => !prevLiked);
+  };
+
+  const handleBuyClick = () => {
+    setIsBuyModalVisible(true);
+  };
+
+  const handleModalClose = () => {
+    setIsBuyModalVisible(false);
+    setInputValue("");
   };
 
   if (!stockData) {
@@ -79,28 +86,18 @@ const StockDetailPage: React.FC = () => {
       {/* 주식 정보 */}
       <div className="p-4">
         <div className="flex items-center justify-between">
-          <div className="flex items-center">
-            <h1 className="text-4xl font-bold text-white text-left ml-4">
-              {stockData.stck_prpr.toLocaleString()}
-            </h1>
-            <span
-              className={`ml-8 mt-2 text-sm font-normal px-2 py-1 rounded-full ${
-                stockData.prdy_ctrt.startsWith("+")
-                  ? "bg-green-900 text-white"
-                  : "bg-green-100 text-black"
-              }`}
-            >
-              {stockData.prdy_ctrt}
-            </span>
-          </div>
-
-          {/* 포트폴리오 추가 버튼 */}
-          <div
-            className="text-white font-bold py-2 px-4 rounded-lg mr-4"
-            onClick={handleAddToPortfolio}
+          <h1 className="text-4xl font-bold text-white text-left ml-4">
+            {stockData.stck_prpr.toLocaleString()}
+          </h1>
+          <span
+            className={`mr-4 mt-2 text-md font-normal px-2 py-1 rounded-full ${
+              stockData.prdy_ctrt.startsWith("+")
+                ? "bg-green-900 text-white"
+                : "bg-green-100 text-black"
+            }`}
           >
-            +
-          </div>
+            {stockData.prdy_ctrt}
+          </span>
         </div>
       </div>
 
@@ -121,39 +118,11 @@ const StockDetailPage: React.FC = () => {
       </div>
 
       {/* 그래프 */}
-      <div className="w-fit mx-auto">
-        <div className="w-[350px] h-32">
-          <ResponsiveContainer width="100%" height="100%">
-            <AreaChart data={chartData}>
-              <defs>
-                <linearGradient id="colorValue" x1="0" y1="0" x2="0" y2="1">
-                  <stop offset="0%" stopColor="#32CD32" stopOpacity={0.8} />
-                  <stop offset="100%" stopColor="#32CD32" stopOpacity={0} />
-                </linearGradient>
-              </defs>
-              <YAxis domain={[adjustedMin, adjustedMax]} hide />
-              <XAxis dataKey="name" hide />
-              <Tooltip
-                cursor={false}
-                contentStyle={{
-                  backgroundColor: "#ffffff",
-                  borderRadius: "10px",
-                  border: "none",
-                }}
-                labelStyle={{ color: "#333" }}
-                itemStyle={{ color: "#333" }}
-              />
-              <Area
-                type="monotone"
-                dataKey="value"
-                stroke="#32CD32"
-                fill="url(#colorValue)"
-                strokeWidth={2}
-              />
-            </AreaChart>
-          </ResponsiveContainer>
-        </div>
-      </div>
+      <StockDetailGraph
+        chartData={chartData}
+        adjustedMin={adjustedMin}
+        adjustedMax={adjustedMax}
+      />
 
       {/* 상세정보, 뉴스 선택 바 */}
       <div className="relative flex justify-center mt-6 mb-4 w-fit bg-green-100 rounded-full mx-auto">
@@ -172,45 +141,40 @@ const StockDetailPage: React.FC = () => {
         ))}
       </div>
 
-      {/* 카드 */}
-      <div className="w-10/12 max-w-md mx-auto p-4 bg-white rounded-2xl shadow-md">
-        {selectedInfoType === "상세정보" ? (
-          <div>
-            {[
-              { label: "전일", value: stockData.stck_prdy_clpr },
-              { label: "시가", value: stockData.stck_oprc },
-              { label: "고가", value: stockData.stck_hgpr },
-              { label: "저가", value: stockData.stck_lwpr },
-              { label: "거래량", value: stockData.acml_vol },
-              { label: "52주 최고", value: stockData.stck_mxpr },
-              { label: "52주 최저", value: stockData.stck_llam },
-            ].map((item, index) => (
-              <div
-                key={index}
-                className="flex justify-between font-extrabold text-gray-700 mb-2 border-b border-gray-300 pb-2"
-              >
-                <span>{item.label}</span>
-                <span>{item.value}</span>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <div>
-            {/* 뉴스 정보 */}
-            <p className="text-gray-700">관련 뉴스 없음.</p>
-          </div>
-        )}
-      </div>
+      {/* 주식 정보 */}
+      {selectedInfoType === "상세정보" && (
+        <StockDetailInfo stockData={stockData} />
+      )}
+
+      {/* 뉴스 */}
+      {selectedInfoType === "뉴스" && <StockNews />}
 
       {/* 매수, 매도 버튼 */}
-      <div className="mt-6 flex justify-between w-11/12 mx-auto">
-        <button className="w-1/2 bg-green-400 text-white py-2 rounded-lg mr-2">
+      <div className="mt-6 flex justify-between w-10/12 mx-auto">
+        <button
+          className="w-1/2 bg-green-400 text-white py-2 rounded-lg mr-2"
+          onClick={handleBuyClick}
+        >
           매수
         </button>
-        <button className="w-1/2 bg-red-400 text-white py-2 rounded-lg ml-2">
-          매도
+        <button
+          className="w-1/2 bg-blue-500 text-white py-2 rounded-lg ml-2"
+          onClick={handleAddToPortfolio}
+        >
+          자동투자하기
         </button>
       </div>
+
+      {/* 매수 모달 */}
+      {isBuyModalVisible && (
+        <StockPurchaseModal
+          inputValue={inputValue}
+          setInputValue={setInputValue}
+          onClose={handleModalClose}
+          stockName={stockData.hts_kor_isnm}
+          stockPrice={stockData.stck_prpr}
+        />
+      )}
     </div>
   );
 };
