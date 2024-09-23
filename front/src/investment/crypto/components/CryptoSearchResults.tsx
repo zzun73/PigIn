678d429da41ce7c2a3674437d47e0424c453f79b
piilll <spchoi1998@naver.com>
2024-09-23 10:11:42 +0900

    Fix : 가상화폐 검색 기능 수정

--- front/src/investment/crypto/components/CryptoSearchResults.tsx
+++ front/src/investment/crypto/components/CryptoSearchResults.tsx
@@ -1,42 +1,41 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import CryptoItem from "../../crypto/components/CryptoItem";
+import { CryptoItemData } from "../../interfaces/CryptoInterface";
 
 interface CryptoSearchResultsProps {
-  filteredCryptos: Array<{
-    name: string;
-    symbol: string;
-    price: number;
-    marketCap: string;
-    volume: string;
-    percentageChange: string;
-    openPrice: number;
-    closePrice: number;
-    high: number;
-    low: number;
-    weeklyPrices: number[];
-    monthlyPrices: number[];
-  }>;
+  filteredCryptos: CryptoItemData[];
 }
 
 const CryptoSearchResults: React.FC<CryptoSearchResultsProps> = ({
   filteredCryptos,
 }) => {
+  const navigate = useNavigate();
+
+  const handleItemClick = (crypto: CryptoItemData) => {
+    navigate(`/investment/cryptocurrency/${crypto.symbol}`, {
+      state: { item: crypto },
+    });
+  };
+
   return (
     <div className="bg-white rounded-2xl p-4 mt-4 shadow-md w-full max-w-md">
       {filteredCryptos.length === 0 ? (
         <p className="text-gray-500">검색 결과가 없습니다.</p>
       ) : (
         filteredCryptos.map((crypto) => (
-          <CryptoItem
+          <div
             key={crypto.symbol}
-            name={crypto.name}
-            price={crypto.price}
-            percentageChange={crypto.percentageChange}
-            data={crypto.weeklyPrices.map((price, index) => ({
-              name: `Day ${index + 1}`,
-              value: price,
-            }))}
-          />
+            className="cursor-pointer"
+            onClick={() => handleItemClick(crypto)}
+          >
+            <CryptoItem
+              name={crypto.name}
+              price={crypto.price}
+              percentageChange={crypto.percentageChange}
+              weeklyPrices={crypto.weeklyPrices}
+            />
+          </div>
         ))
       )}
     </div>
