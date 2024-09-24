b12a7cdd39c387bec43592815043a89b309ffbea
이예지 <lllyyj75@gmail.com>
2024-09-24 11:39:29 +0900

    Fix : conflict 해결

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -14,9 +14,11 @@ import CryptoDetailPage from "./investment/crypto/pages/CryptoMainPage";
 import GoldDetailPage from "./investment/gold/pages/GoldDetailPage";
 import StockSearchPage from "./investment/stock/pages/StockSearchPage";
 import CryptoSearchPage from "./investment/crypto/pages/CryptoSearchPage";
+import StockMainPage from "./investment/stock/pages/StockMainPage";
+import CryptoMainPage from "./investment/crypto/pages/CryptoMainPage";
 import MyPortfolio from "./portfolio/MyPortfolio";
 import AutoInvestment from "./portfolio/AutoInvestment";
-import Quiz from './quiz/Quiz';
+import Quiz from "./quiz/Quiz";
 
 const App: React.FC = () => {
   return (
@@ -27,12 +29,14 @@ const App: React.FC = () => {
           <Route path="/investment" element={<InvestmentLayout />}>
             <Route index element={<InvestmentPage />} />
             <Route path="stock" element={<StockDetailLayout />}>
-              <Route index element={<StockDetailPage />} />
+              <Route index element={<StockMainPage />} />
               <Route path="search" element={<StockSearchPage />} />
+              <Route path=":symbol" element={<StockDetailPage />} />
             </Route>
             <Route path="cryptocurrency" element={<CryptoDetailLayout />}>
-              <Route index element={<CryptoDetailPage />} />
+              <Route index element={<CryptoMainPage />} />
               <Route path="search" element={<CryptoSearchPage />} />
+              <Route path=":symbol" element={<CryptoDetailPage />} />
             </Route>
             <Route path="gold" element={<GoldDetailPage />} />
           </Route>
