b642700581db8667d048f0b71c9fc6d84c3b249b
zzun73 <736300@naver.com>
2024-09-26 00:00:00 +0900

    feat: health-check api 추가

---
+++ back/MySSAFYData/src/main/java/com/ssafy/myssafydata/healthcheck/HealthCheckController.java
@@ -0,0 +1,19 @@
+package com.ssafy.myssafydata.healthcheck;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/health-check")
+public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<String> healthCheck() {
+        log.info("Wallet Health Check!!");
+        return ResponseEntity.ok("Wallet Pong!");
+    }
+}
