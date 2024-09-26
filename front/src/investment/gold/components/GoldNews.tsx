b686fb0c29586ae38eabc07f8c71233b89c12cca
piilll <spchoi1998@naver.com>
2024-09-26 10:07:18 +0900

    Add : 아이템별로 뉴스 데이터 포맷 맞춰서 재작성

--- front/src/investment/gold/components/GoldNews.tsx
+++ front/src/investment/gold/components/GoldNews.tsx
@@ -1,43 +1,26 @@
-import React from "react";
+import React from 'react';
+import newsData from '../../../data/NewsData.json';
 
 const GoldNews: React.FC = () => {
-  const newsData = [
-    {
-      title: "금 가격 상승",
-      date: "09-12",
-    },
-    {
-      title: "안정된 금 거래 시장",
-      date: "09-10",
-    },
-    {
-      title: "중앙은행 금 보유량 증가",
-      date: "09-08",
-    },
-    {
-      title: "금 투자 열풍",
-      date: "09-06",
-    },
-    {
-      title: "금 가격 예측",
-      date: "09-04",
-    },
-  ];
-
   return (
     <div className="w-10/12 max-w-md mx-auto p-4 bg-white rounded-2xl shadow-md h-80 overflow-y-auto">
-      {newsData.map((news, index) => (
-        <div key={index} className="border-b pt-1 pb-2 border-gray-300">
-          <div className="flex justify-between items-center">
-            <p className="text-sm text-gray-600 flex-1 text-left">
-              {news.date}
-            </p>
-            <h3 className="font-bold text-black flex-1 text-right">
-              {news.title}
-            </h3>
+      {newsData.map((news, index) => {
+        const formattedDate = news.Date.slice(5, 10);
+        return (
+          <div key={index} className="border-b pt-1 pb-2 border-gray-300">
+            <div className="flex justify-between items-center">
+              <p className="text-sm text-gray-600 flex-1 text-left">
+                {formattedDate}
+              </p>
+              <h3 className="font-bold text-black flex-1 text-right">
+                <a href={news.Link} target="_blank" rel="noopener noreferrer">
+                  {news.NewsTitle}
+                </a>
+              </h3>
+            </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
