77869c4d976a06e995daf7e3171789b183b228d4
piilll <spchoi1998@naver.com>
2024-09-26 17:41:08 +0900

    Add : 코드 사용 위한 설명 주석 추가

--- front/src/api/test.ts
+++ front/src/api/test.ts
@@ -1,21 +1,29 @@
 import axios from 'axios';
 import axiosInstance from './axiosInstance';
 
+// 응답 인터페이스 형식 정의 - 무슨무슨 데이터 들어가는지
 interface HealthCheckResponse {
   message: string;
 }
 
 export const healthCheck = async (): Promise<HealthCheckResponse> => {
   try {
+    // axiosInstance 사용해 api 호출
     const response =
       await axiosInstance.get<HealthCheckResponse>('api/health-check');
+    // 응답 데이터 반환(성공했을 때)
     return response.data;
+
+    // 오류 췤
   } catch (error: unknown) {
+    // axios의 오류라면 그렇다고 명시
     if (axios.isAxiosError(error)) {
-      console.error('Error during health-check:', error.response?.data);
+      console.error('axios의 오류임:', error.response?.data);
+      // 아니라면 아니라고 명시
     } else {
-      console.error('An unknown error occurred:', error);
+      console.error('axios 잘못 아님 너잘못임:', error);
     }
-    throw new Error('Health check failed.');
+    // 틀렸으니깐 다시 해라
+    throw new Error('다시 해라');
   }
 };
