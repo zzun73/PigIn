281a23f21ed318bcb1ca30c04afadd9bad745cb0
최승현 <csh7099@naver.com>
2024-09-26 17:48:40 +0900

    fix : mms compare

--- back/SSAFYSecurities/src/main/java/com/ssafy/securities/gold/service/GoldServiceImpl.java
+++ back/SSAFYSecurities/src/main/java/com/ssafy/securities/gold/service/GoldServiceImpl.java
@@ -1,7 +1,13 @@
 package com.ssafy.securities.gold.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class GoldServiceImpl implements GoldService {
 
     @Value("${gold.APIKEY}")
