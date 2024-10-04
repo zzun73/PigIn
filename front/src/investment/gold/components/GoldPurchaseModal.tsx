ed18af4e14903074fd5010a99030c334fe49661d
piilll <spchoi1998@naver.com>
2024-10-04 14:57:36 +0900

    fix : 금 매수 모달 형식 변경

--- front/src/investment/gold/components/GoldPurchaseModal.tsx
+++ front/src/investment/gold/components/GoldPurchaseModal.tsx
@@ -17,17 +17,31 @@ const GoldPurchaseModal: React.FC<GoldPurchaseModalProps> = ({
 }) => {
   const handleKeypadClick = (number: string) => {
     setInputValue((prev) => {
-      if (prev.length < 6) {
-        return prev + number;
-      } else {
+      if (prev.length > 4) {
         return prev;
       }
+      if (prev === '00') {
+        return number === '0' || number === '00' ? prev : number + '00';
+      }
+      return prev.slice(0, -2) + number + '00';
     });
   };
 
   const handleBackspace = useCallback(() => {
-    setInputValue((prev) => prev.slice(0, -1));
+    setInputValue((prev) => {
+      if (prev === '00' || prev.length === 3) {
+        return '00';
+      }
+      return prev.slice(0, -3) + '00';
+    });
   }, [setInputValue]);
+
+  const handleAddAmount = (amount: number) => {
+    setInputValue((prev) => {
+      const newValue = parseInt(prev || '0') + amount;
+      return newValue.toString();
+    });
+  };
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
@@ -37,6 +51,7 @@ const GoldPurchaseModal: React.FC<GoldPurchaseModalProps> = ({
     };
 
     window.addEventListener('keydown', handleKeyDown);
+
     return () => {
       window.removeEventListener('keydown', handleKeyDown);
     };
@@ -48,18 +63,20 @@ const GoldPurchaseModal: React.FC<GoldPurchaseModalProps> = ({
   const handleBuyClick = async () => {
     try {
       const response = await tradeGold(inputAmount, 'BUY');
-      console.log('금 매수 성공핑:', response);
+      console.log('Gold purchase successful:', response);
+      onClose();
     } catch (error) {
-      console.error('금 매수 실패핑:', error);
+      console.error('Gold purchase failed:', error);
     }
   };
 
   return (
     <div className="modal-content fixed inset-0 bg-gray-800 bg-opacity-50 flex justify-center items-end z-50">
       <div className="bg-white w-full h-3/4 rounded-t-3xl p-6 relative">
+        {/* 모달 상단 */}
         <div className="flex justify-between items-center mb-4">
           <h1 className="text-2xl font-bold text-black flex justify-center">
-            얼마를 매수하시겠어요?
+            얼마나 매수하시겠어요?
           </h1>
           <div onClick={onClose} className="text-black">
             <CgClose size={24} />
@@ -70,10 +87,11 @@ const GoldPurchaseModal: React.FC<GoldPurchaseModalProps> = ({
           금 현재가 : {goldPrice.toLocaleString()}원
         </div>
 
+        {/* 가격 표시 칸 */}
         <div className="relative flex justify-center mb-6">
           <input
             type="text"
-            value={inputValue}
+            value={inputValue === '00' ? '' : inputValue}
             readOnly
             className={`bg-transparent text-center text-black text-3xl w-3/4 p-2 transition-all ${
               inputValue ? 'border-b border-black' : ''
@@ -88,51 +106,78 @@ const GoldPurchaseModal: React.FC<GoldPurchaseModalProps> = ({
           </div>
         </div>
 
+        {/* 500, 1000, 3000, 5000원 추가 버튼 */}
         <div className="flex justify-center space-x-4 mb-6">
           {[500, 1000, 3000, 5000].map((amount) => (
             <button
               key={amount}
-              className="bg-customDarkGreen p-2 text-sm text-white rounded-full transition-colors"
-              onClick={() =>
-                setInputValue((prev) =>
-                  (parseInt(prev || '0') + amount).toString()
-                )
-              }
+              className="bg-customDarkGreen p-2 text-sm rounded-full text-white transition-colors"
+              onClick={() => handleAddAmount(amount)}
             >
               +{amount.toLocaleString()}원
             </button>
           ))}
         </div>
 
+        {/* 키패드 */}
         <div className="grid grid-cols-3 gap-4 justify-center">
-          {['1', '2', '3', '4', '5', '6', '7', '8', '9'].map((num) => (
+          {[
+            '1',
+            '2',
+            '3',
+            '4',
+            '5',
+            '6',
+            '7',
+            '8',
+            '9',
+            '00',
+            '0',
+            'backspace',
+          ].map((key) => (
             <button
-              key={num}
+              key={key}
               className="bg-transparent text-black text-2xl p-4 rounded-lg transition-colors w-full h-20"
-              onClick={() => handleKeypadClick(num)}
+              onClick={() =>
+                key === 'backspace' ? handleBackspace() : handleKeypadClick(key)
+              }
             >
-              {num}
+              {key === 'backspace' ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  className="w-6 h-6 ml-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M19 12H5M12 19l-7-7 7-7"
+                  />
+                </svg>
+              ) : (
+                key
+              )}
             </button>
           ))}
-          <button
-            className="bg-transparent text-black text-2xl p-4 rounded-lg transition-colors w-full h-20 col-span-3"
-            onClick={() => handleKeypadClick('0')}
-          >
-            0
-          </button>
         </div>
 
-        <button
-          className={`w-full py-3 rounded-md text-lg font-bold ${
-            inputValue === '00'
-              ? 'bg-gray-300 text-white cursor-not-allowed'
-              : 'bg-green-500 text-white'
-          }`}
-          disabled={inputValue === '00'}
-          onClick={handleBuyClick}
-        >
-          매수하기
-        </button>
+        {/* 매수하기 버튼 */}
+        <div className="flex justify-center">
+          <button
+            className={`w-full py-3 rounded-md text-lg font-bold ${
+              inputValue === '00'
+                ? 'bg-gray-300 text-white cursor-not-allowed'
+                : 'bg-green-500 text-white'
+            }`}
+            disabled={inputValue === '00'}
+            onClick={handleBuyClick}
+          >
+            매수하기
+          </button>
+        </div>
       </div>
     </div>
   );
