0a3d8a78db72aae635491e756e799b7cce372eed
zzun73 <736300@naver.com>
2024-10-05 17:15:19 +0900

    chore: MM message 수정

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -1,24 +1,26 @@
 def call(String status) {
     def authorID = sh(script: "git show -s --pretty=%an", returnStdout: true).trim()
     def commitMessage = sh(script: "git log -1 --pretty=%s", returnStdout: true).trim()
+    def buildStage = env.STAGE_NAME ?: "N/A"
     def buildDetails = """
-        **프로젝트**: ${env.JOB_NAME}
-        **빌드 번호**: #${env.BUILD_NUMBER}
-        **실행자**: ${authorID}
-        **커밋**: "${commitMessage}"
-        **소요 시간**: ${currentBuild.durationString}
+    📌 __프로젝트__: `${env.JOB_NAME}`
+    🔢 __빌드 번호__: `#${env.BUILD_NUMBER}`
+    👤 __실행자__: `${authorID}`
+    📝 __커밋 메시지__: "${commitMessage}"
+    📶 __빌드 스테이지__: `${buildStage}`
+    ⏱ __소요 시간__: `${currentBuild.durationString}`
     """
 
     if (status == 'success') {
         mattermostSend(
             color: 'good',
-            message: ":jenkins_cute_flip:  **Build Success** :jenkins_cute_flip:\n" + buildDetails
+            message: "🎉 __Build Success__ 🎉\n${buildDetails}"
         )
     } else if (status == 'failure') {
-        def errorLog = currentBuild.rawBuild.getLog(10).join('\n')
+        def errorLog = currentBuild.rawBuild.getLog(50).join('\n')
         mattermostSend(
             color: 'danger',
-            message: ":jenkins5: **Build Failed** :jenkins5:\n" + buildDetails + "\n**오류**:\n```${errorLog}```"
+            message: "❌ __Build Failed__ ❌\n${buildDetails}\n\n🔴 __오류 스테이지__: `${buildStage}`\n\n🔴 __오류 로그__:\n```${errorLog}```"
         )
     }
 }
