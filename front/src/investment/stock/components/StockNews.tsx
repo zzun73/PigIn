906f4ebdd5878b8787b8c6b3936fe89371628dda
piilll <spchoi1998@naver.com>
2024-10-10 09:52:42 +0900

    fix: 뉴스 데이터 날짜 레이아웃 수정

--- front/src/investment/stock/components/StockNews.tsx
+++ front/src/investment/stock/components/StockNews.tsx
@@ -28,7 +28,7 @@ const StockNews: React.FC<StockNewsProps> = ({ stockId }) => {
         <p>뉴스 없음</p>
       ) : (
         newsData.map((news, index) => {
-          const formattedDate = news.Date.slice(6, 11);
+          const formattedDate = news.Date.slice(5, 10);
           return (
             <div key={index} className="border-b pt-1 pb-2 border-gray-300">
               <div className="flex justify-between items-center">
